89dcf5cbb57298c4a20f67b191c86bdd5258a420
Ruan Victor <rvsilva@thoughtworks.com>
2018-07-23 19:00:26 -0300

    Add getters in model

--- src/main/java/model/ClienteModel.java
+++ src/main/java/model/ClienteModel.java
@@ -2,9 +2,11 @@ package model;
 
 import lombok.AccessLevel;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.experimental.FieldDefaults;
 
 @Builder
+@Getter
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class ClienteModel extends EnderecoModel {
 
--- src/main/java/model/EnderecoModel.java
+++ src/main/java/model/EnderecoModel.java
@@ -1,10 +1,10 @@
 package model;
 
 import lombok.AccessLevel;
-import lombok.Builder;
+import lombok.Data;
 import lombok.experimental.FieldDefaults;
 
-@Builder
+@Data
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class EnderecoModel {
     String endereco;
