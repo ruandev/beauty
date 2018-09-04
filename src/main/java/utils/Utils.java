58fdbf28a90c0f45b4492e77da98881bcc30deb5
Ruan Victor <ruan.techno@gmail.com>
2018-09-03 23:33:10 -0300

    Start CRUD Agendamento

--- src/main/java/utils/Utils.java
+++ src/main/java/utils/Utils.java
@@ -1,9 +1,14 @@
 package utils;
 
 import java.sql.Timestamp;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.format.ResolverStyle;
 import java.util.Date;
 
 public class Utils {
@@ -18,5 +23,17 @@ public class Utils {
 
     public static LocalDateTime convetTimestampToLocalDateTime(Timestamp dataHora){
         return dataHora.toLocalDateTime();
+    }
+    
+     public static LocalDateTime convetDateAndStringHourToLocalDateTime(Date data, String hora){
+        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        String dataHora = dateFormat.format(data)+" "+hora;
+        System.out.println(dataHora);
+        DateTimeFormatter formatter = new DateTimeFormatterBuilder()
+            .parseStrict()
+            .appendPattern("dd/MM/uuuu HH:mm")
+            .toFormatter()
+            .withResolverStyle(ResolverStyle.STRICT);
+        return LocalDateTime.parse(dataHora, formatter);
     }
 }
